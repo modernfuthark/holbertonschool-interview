a61043068acebb6bff13dfcd8a4c55fdf4ff7f24
modernfuthark <timothys1738@gmail.com>
2020-12-09 00:47:05 -0500

    [^] pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,14 +1,18 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
+""" Lockboxes file """
+
 
 def canUnlockAll(boxes):
+    """ Checks if there are the required keys in the given boxes """
     if boxes:
-        keys = [0] # Box 0 is unlocked
-        required = [i[0] for i in enumerate(boxes)] # Gets an array equal to length
+        keys = [0]  # Box 0 is unlocked
+        # Gets the length of array boxes as an array
+        required = [i[0] for i in enumerate(boxes)]
 
         for i in range(0, len(boxes)):
             for v in boxes[i]:
                 if v not in keys and v < len(boxes) and v != i:
-                    keys.append(v);
+                    keys.append(v)
                     i = v
 
         # print(f'Required: {required}\nHas: {sorted(keys)}')
