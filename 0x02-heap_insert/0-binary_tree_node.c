d74b6c515e53e7c60c37dc14c93644b4dc0b93b5
modernfuthark <timothys1738@gmail.com>
2020-12-22 19:15:38 -0500

    [N] Bulk pushed files

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Parent node
+ * @value: Value inside of new node
+ * Return: Pointer to new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	return (new);
+}
---
+++ 0x02-heap_insert/README.md
@@ -0,0 +1,5 @@
+**Interview - Heap Insert**
+Tim Sninsky\
+12/22/20
+---
+Directory for the `Interview - Heap Insert` project.
