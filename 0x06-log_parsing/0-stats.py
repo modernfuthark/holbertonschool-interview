6525db97b3d51a7816fa4d0481e922f247457679
modernfuthark <timothys1738@gmail.com>
2021-01-28 00:15:33 -0500

    [^] Moved printing to its own function, fixed loop going over 1

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,35 +3,35 @@
 import sys
 import re
 
+def printStats():
+    print("File size: {}".format(totalSize))
+    for k in codes:
+        if codes[k] != 0:
+            print("{}: {}".format(k, codes[k]))
 
 counter = 0
 codes = {200: 0, 301: 0, 400: 0,
          401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 tester = '#.#.#.# - [#-#-# #:#:#.#] "GET /projects/# HTTP/#.#" # #'
-gfilesize = 0  # Global file size
+totalSize = 0  # Global file size
 
 try:
     for L in sys.stdin:
-        #if re.sub('\d+', '#', L.rstrip('\n')) == tester:
-        if 1 == 1:
+        if re.sub('\d+', '#', L.rstrip('\n')) == tester:
             # print(counter + 1)  # +1 for readability
             scode = int(re.match('(?:.*?\d+){14}.*?(\d+)',
                         L.rstrip('\n')).group(1))
             fsize = int(re.findall('\d+$', L.rstrip('\n'))[0])
+            totalSize += fsize
 
             if scode in codes:
                 codes[scode] += 1
-                gfilesize += fsize
-            if counter < 10:
+            if counter < 9:
                 counter += 1
             else:
-                print("File size: {}".format(gfilesize))
-                for k in codes:
-                    if codes[k] != 0:
-                        print("{}: {}".format(k, codes[k]))
+                printStats()
                 counter = 0
+    printStats()
+
 except KeyboardInterrupt:
-    print("File size: {}".format(gfilesize))
-    for k in codes:
-        if codes[k] != 0:
-            print("{}: {}".format(k, codes[k]))
+    printStats()
