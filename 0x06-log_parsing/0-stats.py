cc3f3953b8ababb139c88c46dc28cf5cac01a6ae
modernfuthark <timothys1738@gmail.com>
2021-01-28 00:05:31 -0500

    [^] Moved code out of main function

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
+""" Reads stdin and gathers data, printing data every 10 reads """
 import sys
 import re
-import signal
 
 
-def Main():
-    """ Main function """
+counter = 0
+codes = {200: 0, 301: 0, 400: 0,
+         401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+tester = '#.#.#.# - [#-#-# #:#:#.#] "GET /projects/# HTTP/#.#" # #'
+gfilesize = 0  # Global file size
 
-    # Signal catching
-    def sig(sNum, sFrame):
-        print("File size: {}".format(gfilesize))
-        for k in codes:
-            if codes[k] != 0:
-                print("{}: {}".format(k, codes[k]))
-    signal.signal(signal.SIGINT, sig)
-
-    # Start parsing function...
-    counter = 0
-    codes = {200: 0, 301: 0, 400: 0,
-             401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    tester = '#.#.#.# - [#-#-# #:#:#.#] "GET /projects/# HTTP/#.#" # #'
-    gfilesize = 0  # Global file size
-
+try:
     for L in sys.stdin:
         if re.sub('\d+', '#', L.rstrip('\n')) == tester:
             # print(counter + 1)  # +1 for readability
-
             scode = int(re.match('(?:.*?\d+){14}.*?(\d+)',
                         L.rstrip('\n')).group(1))
             fsize = int(re.findall('\d+$', L.rstrip('\n'))[0])
@@ -41,6 +29,8 @@ def Main():
                     if codes[k] != 0:
                         print("{}: {}".format(k, codes[k]))
                 counter = 0
-
-if __name__ == "__main__":
-        Main()
+except KeyboardInterrupt:
+    print("File size: {}".format(gfilesize))
+    for k in codes:
+        if codes[k] != 0:
+            print("{}: {}".format(k, codes[k]))
