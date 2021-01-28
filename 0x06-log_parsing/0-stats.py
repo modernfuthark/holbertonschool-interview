278a70f5e6f0171d38b21cb117a7d17aedf31357
modernfuthark <timothys1738@gmail.com>
2021-01-27 23:52:39 -0500

    [^] Pep8

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -18,16 +18,17 @@ def Main():
     # Start parsing function...
     counter = 0
     codes = {200: 0, 301: 0, 400: 0,
-               401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+             401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     tester = '#.#.#.# - [#-#-# #:#:#.#] "GET /projects/# HTTP/#.#" # #'
     gfilesize = 0  # Global file size
 
     for L in sys.stdin:
         if re.sub('\d+', '#', L.rstrip('\n')) == tester:
-            #print(counter + 1)  # +1 for readability
+            # print(counter + 1)  # +1 for readability
 
-            scode = int(re.match('(?:.*?\d+){14}.*?(\d+)', L.rstrip('\n')).group(1))
-            fsize = int(re.findall('\d+$', L.rstrip('\n'))[0])  # Really doesn't like using .match
+            scode = int(re.match('(?:.*?\d+){14}.*?(\d+)',
+                        L.rstrip('\n')).group(1))
+            fsize = int(re.findall('\d+$', L.rstrip('\n'))[0])
 
             if scode in codes:
                 codes[scode] += 1
