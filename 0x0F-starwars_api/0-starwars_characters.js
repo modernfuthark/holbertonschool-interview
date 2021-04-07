587d6c50bae1970123ce7c3048a095d3637532e7
modernfuthark <timothys1738@gmail.com>
2021-04-07 18:08:08 -0400

    [^] Implemented async and promise

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,13 +4,19 @@ const req = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${id}/`;
 
-/* console.log(url); */
-req(url, (err, res, body) => {
+req(url, async (err, res, body) => {
   if (err) throw err;
-  for (const charUrl of JSON.parse(body).characters) {
-    req(charUrl, (err, res, body) => {
-      if (err) throw err;
-      console.log(JSON.parse(body).name);
+  for (const cUrl of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      req(cUrl, (err, res, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          /* console.log(cUrl) */
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
     });
   }
 });
