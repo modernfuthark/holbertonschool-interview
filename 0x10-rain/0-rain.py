c8da3c2779d392738b30619c6c09b386d525cc5d
modernfuthark <timothys1738@gmail.com>
2021-04-15 18:10:13 -0400

    [N] Added rain file

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+0-rain
+"""
+
+
+def rain(walls):
+    """ rain function """
+    size = len(walls)
+    totalWater = 0
+
+    if size <= 0:
+        return 0
+
+    lastWall = 0
+    for i in range(size):
+        # Bigger wall found, set lastWall to it
+        if walls[i] >= walls[lastWall]:
+            lastWall = i
+        else:
+            # Add "size" of pit between lastWall and current index
+            totalWater += walls[lastWall] - walls[i]
+
+    if lastWall < size - 1:
+        # Backtrack through list, since last wall wasn't at the end of the list
+        # Remove last added water - 1 as compensation
+        totalWater -= totalWater - 1
+        lastWallcp = lastWall
+        lastWall = size - 1
+        # Iterate backwards from end of list to lastWall
+        for i in range(size - 1, lastWallcp, -1):
+            # Check if the current wall is greater than lastWall
+            if walls[i] >= walls[lastWall]:
+                lastWall = i
+            else:
+                # Add "size" of pit between lastWall and current index
+                totalWater += walls[lastWall] - walls[i]
+
+    return totalWater
