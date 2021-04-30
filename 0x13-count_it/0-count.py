eac0870b3f70e05558ca852c7febe0e37cc59ed9
modernfuthark <timothys1738@gmail.com>
2021-04-30 02:56:05 -0400

    [+] Added comments

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -10,6 +10,8 @@ import re
 def count_words(subreddit, word_list, total={}, aft=None):
     """ count occurences of words """
 
+    # On the first call of the function,
+    # create dictionary of words with a count of 0
     if len(total) == 0:
         for word in word_list:
             total[word] = 0
@@ -22,7 +24,7 @@ def count_words(subreddit, word_list, total={}, aft=None):
                        headers={"User-Agent": "0x13-count_it_TimSninsky"})
 
     if req.status_code != 200:
-        print(req.status_code)
+        #print(req.status_code)
         return
 
     data = req.json()['data']
@@ -30,14 +32,18 @@ def count_words(subreddit, word_list, total={}, aft=None):
     for post in data['children']:
         title = post['data']['title']
         for k,v in total.items():
+            # Create pattern with key and a negative lookahead
             pattern = k + "(?!\w+)"
+            # Get all instances of pattern in title, and add to dictionary
             result = re.findall(pattern, title, flags=re.IGNORECASE)
             total[k] += len(result)
 
+    # Check if there's an after, and recurse if so
     aft = data['after']
     if aft is not None:
         return count_words(subreddit, word_list, total, aft)
     else:
+        # Sort dictionary by the key's values
         sorted_totals = reversed(sorted(total.items(), key=lambda kv: kv[1]))
         for item in sorted_totals:
             if item[1] > 0:
