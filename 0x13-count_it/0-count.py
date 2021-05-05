cc0bbeb41a225e2de30ee82a48d34ef03402117b
modernfuthark <timothys1738@gmail.com>
2021-05-05 18:53:27 -0400

    [+] added a newline print??

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -24,6 +24,7 @@ def count_words(subreddit, word_list, total={}, aft=None):
                        headers={"User-Agent": "0x13-count_it_TimSninsky"})
 
     if req.status_code != 200:
+        print()
         return
 
     data = req.json()['data']
