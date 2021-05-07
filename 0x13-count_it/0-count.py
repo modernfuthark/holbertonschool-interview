f56cd14e02fcc00b60215f9a9f2fee6d250cec41
modernfuthark <timothys1738@gmail.com>
2021-05-07 00:35:17 -0400

    [^] Updates

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -6,7 +6,7 @@
 import requests
 
 
-def count_words(subreddit, word_list, total={}, aft=None):
+def count_words(subreddit, word_list, total={}, aft=""):
     """ count occurences of words """
 
     # On the first call of the function,
@@ -16,11 +16,11 @@ def count_words(subreddit, word_list, total={}, aft=None):
             total[word] = 0
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    if aft:
+    if aft != "":
         url += "?after={}".format(aft)
 
     req = requests.get(url, allow_redirects=False,
-                       headers={"User-Agent": "0x13-count_it_TimSninsky"})
+                       headers={"User-Agent": "PlaceholderAgent"})
 
     if req.status_code != 200:
         return None
@@ -37,20 +37,17 @@ def count_words(subreddit, word_list, total={}, aft=None):
     # Check if there's an after, and recurse if so
     aft = data['after']
     if aft is not None:
-        return count_words(subreddit, word_list, total, aft)
+        count_words(subreddit, word_list, total, aft)
     else:
         # Combine duplicates into a lowercase key
         sortedTotals = {}
         for k, v in total.items():
-            if v > 0:
+            if v != 0:
                 low = k.lower()
                 if low not in sortedTotals:
                     sortedTotals[low] = v
                 else:
                     sortedTotals[low] += v
-        if len(sortedTotals) == 0:
-            print("")
-            return None
 
         # Sort dictionary by value
         sortedTotals = sorted(sortedTotals.items(),
@@ -59,4 +56,4 @@ def count_words(subreddit, word_list, total={}, aft=None):
         # Print list
         for k, v in sortedTotals:
                 print("{}: {}".format(k, v))
-        return sortedTotals
+        return
