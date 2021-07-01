104233e0c1c6bdb679feea7d3c70df24234de0b5
modernfuthark <timothys1738@gmail.com>
2021-07-01 16:21:24 -0400

    [N] Added files for interview project

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,92 @@
+#include "list.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_begin - Adds a node at the start of @list
+ * @list: List to add to
+ * @str: String of new node
+ * Return: Address of new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *newNode, *head, *tail;
+
+	if (!str)
+		return (NULL);
+
+	newNode = malloc(sizeof(List));
+	if (!newNode)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	if (!newNode->str)
+		return (NULL);
+
+	newNode->next = *(list);
+	newNode->prev = NULL;
+
+	if (!*list)
+	{
+		newNode->next = newNode;
+		newNode->prev = newNode;
+	}
+	else
+	{
+		head = *list;
+		tail = (*list)->prev;
+
+		head->prev = newNode;
+		tail->next = newNode;
+
+		newNode->next = head;
+		newNode->prev = tail;
+	}
+
+	*list = newNode;
+	return (newNode);
+}
+
+/**
+ * add_node_end - Adds a node at the end of @list
+ * @list: List to add to
+ * @str: String of new node
+ * Return: Address of new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *newNode, *head, *tail;
+
+	if (!str)
+		return (NULL);
+
+	newNode = malloc(sizeof(List));
+	if (!newNode)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	if (!newNode->str)
+		return (NULL);
+
+	newNode->next = *(list);
+	newNode->prev = NULL;
+
+	if (!*list)
+	{
+		newNode->next = newNode;
+		newNode->prev = newNode;
+		*list = newNode;
+	}
+	else
+	{
+		head = *list;
+		tail = (*list)->prev;
+
+		head->prev = newNode;
+		tail->next = newNode;
+
+		newNode->next = head;
+		newNode->prev = tail;
+	}
+	return (newNode);
+}
---
+++ 0x1A-double_circular_linked_list/README.md
@@ -0,0 +1,15 @@
+# **Interview - Double Circular Linked List**
+Tim Sninsky\
+06/30/21
+### Directory for the `Interview Double Circular Linked List` project.
+---
+<!-- Task-->
+> Create a file that can create a new node at the beginning or end of a doubly linked list
+
+`add_node_end(List **list, char *str)`
+
+Returns the address of a new node at the end of `list`, or `NULL` on failure.
+
+`add_node_beginning(List **list, char *str)`
+
+Returns the address of a new node at the beginning of `list`, or `NULL` on failure.
