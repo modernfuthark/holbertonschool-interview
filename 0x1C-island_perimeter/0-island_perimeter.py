f30a9220fbabc93b9eb91893ed11ab887eb0ab20
modernfuthark <timothys1738@gmail.com>
2021-07-15 19:38:47 -0400

    [^] Added out of range checks

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -9,16 +9,22 @@ def island_perimeter(grid):
     perim = 0
 
     for i in range(0, len(grid)):
-        for j in range(0, len(grid[i])):
+        for j in range(0, len(grid[0])):
             # print(f"checking [{i}][{j}]")
             if grid[i][j] == 1:
                 # land found
+
                 if grid[i][j - 1] == 0:
                     perim += 1
-                if grid[i][j + 1] == 0:
-                    perim += 1
+
+                if j + 1 > len(grid[0]):
+                    if grid[i][j + 1] == 0:
+                        perim += 1
+
                 if grid[i - 1][j] == 0:
                     perim += 1
-                if grid[i + 1][j] == 0:
-                    perim += 1
+
+                if i + 1 > len(grid):
+                    if grid[i + 1][j] == 0:
+                        perim += 1
     return perim
