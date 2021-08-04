ad015d5d025c1700cb88efc65f310a1b0d7e808e
modernfuthark <timothys1738@gmail.com>
2021-08-04 18:50:02 -0400

    [N] Added files

---
+++ 0x1F-pascal_triagnel/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle - Create a Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    result = []
+
+    for row in range(0, n):
+        inner = [1 for _ in range(row + 1)]
+        result.append(inner)
+        if row > 1:
+            for col in range(1, len(inner) - 1):
+                result[row][col] =\
+                    (result[row - 1][col - 1]) + (result[row - 1][col])
+
+    return result
---
+++ 0x1F-pascal_triagnel/README.md
@@ -0,0 +1,11 @@
+# **Interview - Pascal's Triangle**
+Tim Sninsky\
+08/04/21
+### Directory for the `Interview - Pascal's Triangle` project.
+---
+<!-- task-->
+> Create a function that'll create and return a Pascal Triangle of `n` size
+
+`pascal_triangle(n)`
+
+Returns a pascal triangle of size `n`, or an empty list if `n` is <= 0.
