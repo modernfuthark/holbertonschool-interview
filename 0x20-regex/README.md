8c81d4102197102cb412ea5a9ddd39fe0be0eb01
modernfuthark <timothys1738@gmail.com>
2021-08-10 23:26:06 -0400

    [N] Added files

---
+++ 0x20-regex/README.md
@@ -0,0 +1,13 @@
+# **Interview - RegEx**
+Tim Sninsky\
+08/10/21
+### Directory for the `Interview - RegEx` project.
+---
+<!-- task-->
+> Create a function that'll compair a pattern against a string.
+
+`pascal_triangle({string}, {pattern})`
+
+`{pattern}` can contain `*` or `.` as special characters.
+
+Returns 1 if it is a match, 0 otherwise.
---
+++ 0x20-regex/regex.c
@@ -0,0 +1,47 @@
+#include "regex.h"
+#include <stdio.h>
+
+int recursive_checker(const char *s, const char *p);
+
+/**
+ * regex_match - Diet regex function, only supports * and .
+ * @str: String to check
+ * @pattern: Pattern to check against
+ * Return: 1 if the pattern matches, 0 if not
+ */
+
+int regex_match(char const *str, char const *pattern)
+{
+	return (recursive_checker(str, pattern));
+}
+
+/**
+ * recursive_checker - Performs a recursive pattern check against a string
+ * @s: String to check
+ * @p: Pattern to match against
+ * Return: 1 if the pattern matches, 0 if not
+ */
+int recursive_checker(const char *s, const char *p)
+{
+	if (*(s + 1) == '\0' && *(p + 1) == '\0' && (*p == *s || *p == '.'))
+		return (1);
+
+	if (*(p + 1) == '*' && (*p != *s && *p != '.'))
+		return (recursive_checker(s, p + 2));
+
+	if ((*(s + 1) == '\0' && *p != '*') || *p == '\0')
+		return (0);
+
+	if (*s == *p || *p == '.')
+		return (recursive_checker(s + 1, p + 1));
+	else if (*p == '*' && *s != '\0')
+	{
+		if (*(p - 1) == *s || *(p - 1) == '.')
+			return (recursive_checker(s + 1, p));
+		else
+			return (recursive_checker(s + 1, p + 1));
+	}
+	if (*s == '\0')
+		return (1);
+	return (0);
+}
